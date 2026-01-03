ed62d40d5fb8a7776a909c95e24d4aec96d24cf0
LeonFelipeCordero <zolopretroriano@gmail.com>
2026-01-03 18:04:49 +0100

    test: refactor e2e tests

--- sintropy_engine/test/e2e/single_channel_fifo_polling_test.exs
+++ sintropy_engine/test/e2e/single_channel_fifo_polling_test.exs
@@ -1,28 +1,18 @@
 defmodule SintropyEngine.SingleChannelFifoPollingE2eTest do
   use SintropyEngineWeb.ConnCase
 
-  @fifo_channel_attrs %{
-    name: "fifo_test_channel",
-    channel_type: "QUEUE",
-    routing_keys: [%{routing_key: "test.fifo"}],
-    queue: %{consumption_type: "FIFO"}
-  }
-
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
   describe "E2E test for testing a FIFO channel" do
+    import SintropyEngine.E2eFixtures
+
     test "full E2E flow: create channel, publish message, poll, and dequeue via API", %{
       conn: conn
     } do
-      conn = post(conn, ~p"/api/channels", channel: @fifo_channel_attrs)
-      %{"id" => channel_id} = json_response(conn, 201)["data"]
-
-      conn =
-        post(conn, ~p"/api/producers", producer: %{name: "test_producer", channel_id: channel_id})
-
-      %{"id" => producer_id} = json_response(conn, 201)["data"]
+      channel_id = create_fifo_channel(conn)
+      producer_id = create_create_producer(conn, channel_id)
 
       message_attrs = %{
         channel_id: channel_id,
@@ -34,34 +24,30 @@ defmodule SintropyEngine.SingleChannelFifoPollingE2eTest do
         timestamp: DateTime.now!("Etc/UTC") |> DateTime.to_iso8601()
       }
 
-      conn = post(conn, ~p"/api/messages", message: message_attrs)
-      %{"id" => message_id} = json_response(conn, 201)["data"]
+      message_id = send_message(conn, message_attrs)
 
-      conn =
-        post(conn, ~p"/api/messages/poll", %{
+      polled_messages =
+        poll_messages(conn, %{
           channel_id: channel_id,
           routing_key: "test.fifo",
           polling_count: 1
         })
 
-      polled_messages = json_response(conn, 200)["data"]
       assert length(polled_messages) == 1
       polled_message = List.first(polled_messages)
       assert polled_message["id"] == message_id
       assert polled_message["status"] == "IN_FLIGHT"
 
-      conn =
-        post(conn, ~p"/api/messages/poll", %{
+      polled_messages_again =
+        poll_messages(conn, %{
           channel_id: channel_id,
           routing_key: "test.fifo",
           polling_count: 1
         })
 
-      polled_messages_again = json_response(conn, 200)["data"]
       assert polled_messages_again == []
 
-      conn = delete(conn, ~p"/api/messages/#{message_id}")
-      assert response(conn, 204)
+      dequeue_message(conn, message_id)
 
       assert_error_sent 404, fn ->
         get(conn, ~p"/api/messages/#{message_id}")
@@ -69,13 +55,8 @@ defmodule SintropyEngine.SingleChannelFifoPollingE2eTest do
     end
 
     test "publish two messages and poll both in a single call", %{conn: conn} do
-      conn = post(conn, ~p"/api/channels", channel: @fifo_channel_attrs)
-      %{"id" => channel_id} = json_response(conn, 201)["data"]
-
-      conn =
-        post(conn, ~p"/api/producers", producer: %{name: "test_producer", channel_id: channel_id})
-
-      %{"id" => producer_id} = json_response(conn, 201)["data"]
+      channel_id = create_fifo_channel(conn)
+      producer_id = create_create_producer(conn, channel_id)
 
       message_attrs_1 = %{
         channel_id: channel_id,
@@ -83,7 +64,6 @@ defmodule SintropyEngine.SingleChannelFifoPollingE2eTest do
         routing_key: "test.fifo",
         message: "First test message",
         headers: "Content-Type: text/plain",
-        status: "READY",
         timestamp: DateTime.now!("Etc/UTC") |> DateTime.to_iso8601()
       }
 
@@ -93,39 +73,28 @@ defmodule SintropyEngine.SingleChannelFifoPollingE2eTest do
         routing_key: "test.fifo",
         message: "Second test message",
         headers: "Content-Type: text/plain",
-        status: "READY",
         timestamp: DateTime.now!("Etc/UTC") |> DateTime.to_iso8601()
       }
 
-      conn = post(conn, ~p"/api/messages", message: message_attrs_1)
-      %{"id" => message_id_1} = json_response(conn, 201)["data"]
+      message_id_1 = send_message(conn, message_attrs_1)
+      message_id_2 = send_message(conn, message_attrs_2)
 
-      conn = post(conn, ~p"/api/messages", message: message_attrs_2)
-      %{"id" => message_id_2} = json_response(conn, 201)["data"]
-
-      conn =
-        post(conn, ~p"/api/messages/poll", %{
-          channel_id: channel_id,
-          routing_key: "test.fifo",
-          polling_count: 2
-        })
-
-      polled_messages = json_response(conn, 200)["data"]
-      assert length(polled_messages) == 2
+      polled_messages =
+        poll_messages(
+          conn,
+          %{
+            channel_id: channel_id,
+            routing_key: "test.fifo",
+            polling_count: 2
+          }
+        )
 
       polled_message_ids = Enum.map(polled_messages, & &1["id"])
-      assert message_id_1 in polled_message_ids
-      assert message_id_2 in polled_message_ids
+      assert message_id_1 == Enum.at(polled_message_ids, 0)
+      assert message_id_2 == Enum.at(polled_message_ids, 1)
 
-      Enum.each(polled_messages, fn message ->
-        assert message["status"] == "IN_FLIGHT"
-      end)
-
-      conn = delete(conn, ~p"/api/messages/#{message_id_1}")
-      assert response(conn, 204)
-
-      conn = delete(conn, ~p"/api/messages/#{message_id_2}")
-      assert response(conn, 204)
+      dequeue_message(conn, message_id_1)
+      dequeue_message(conn, message_id_2)
     end
   end
 end
